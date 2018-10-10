a1ef8f56b92962d9566bf93f7f999c0b86119b62
tddgroup <tddgroup@hotmail.com>
2018-10-10 18:32:28 +0200

    flag deletion

--- src/Soundex.cpp
+++ src/Soundex.cpp
@@ -2,22 +2,17 @@
 
 std::string Soundex::soundex(std::string s)
 {
-    bool flag[6] = {};
     std::string tmp(1,s[0]);
     for(size_t i = 1; i < s.size(); i++)
     {
         switch(s.at(i)){
-        case 'd': tmp.append("3"); break;
         case 'm': tmp.append("5"); break;
         case 'b':
         case 'f':
         case 'p':
         case 'v':
-            if(!flag[0])
-            {
-                flag[0] = true;
-                tmp.append("1");
-            }   break;
+            tmp.append("1");
+            break;
         case 'c':
         case 'g':
         case 'j':
@@ -26,13 +21,13 @@ std::string Soundex::soundex(std::string s)
         case 's':
         case 'x':
         case 'z':
-            if(!flag[1])
-            {
-                flag[1] = true;
-                tmp.append("2");
-            }   break;
+            tmp.append("2");
+            break;
+        case 'd': tmp.append("3"); break;
 
         }
+        if(tmp[tmp.size()-1] == tmp[tmp.size()-2])
+            tmp.erase(tmp.size()-1, 1);
 
     }
     return tmp;
