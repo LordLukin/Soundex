c9732d6c5b55d0037241526777014c9be0d4536f
tddgroup <tddgroup@hotmail.com>
2018-10-10 17:49:28 +0200

    dropExcluded

--- src/Soundex.cpp
+++ src/Soundex.cpp
@@ -2,8 +2,15 @@
 
 std::string Soundex::soundex(std::string s)
 {
-    if (s.size() != 3)
-        return std::string(1,s[0]) ;
-
-    else return "R3";
+    if (s.size() == 1)
+        return std::string(1,s[0]);
+    std::string tmp(1,s[0]);
+    for(size_t i = 1; i < s.size(); i++)
+    {
+        switch(s.at(i)){
+        case 'a':  break;
+        case 'd': tmp.append("3"); break;
+        }
+    }
+    return tmp;
 }
--- test/SoundexTests.cpp
+++ test/SoundexTests.cpp
@@ -6,7 +6,6 @@ struct SoundexTests : public ::testing::Test
     Soundex s;
 };
 
-
 TEST_F(SoundexTests, assertThatSoundexForRoIsR)
 {
     ASSERT_EQ(s.soundex("Ro"),"R");
@@ -17,9 +16,19 @@ TEST_F(SoundexTests, assertThatSoundexForRodIsR3)
     ASSERT_EQ(s.soundex("Rod"),"R3");
 }
 
-
 TEST_F(SoundexTests, assertThatSoundexForFirstLetterIsThatLetter)
 {
     ASSERT_EQ(s.soundex("J"),"J");
     ASSERT_EQ(s.soundex("R"),"R");
+}
+
+TEST_F(SoundexTests, verifyNonConsolantsIsRemove)
+{
+    ASSERT_EQ(s.soundex("Raa"),"R");
+    ASSERT_EQ(s.soundex("Goo"),"G");
+
+}
+TEST_F(SoundexTests, dropExcluded)
+{
+    ASSERT_EQ(s.soundex("Aaeiouyhw"),"A");
 }
