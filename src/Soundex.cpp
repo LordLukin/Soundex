72e8e18de9e67ca91b68c14b33b3dd9ecb0e073e
Ziobron <lukasz.ziobron@nokia.com>
2019-09-10 20:50:38 +0200

    Refactor

--- src/Soundex.cpp
+++ src/Soundex.cpp
@@ -1,44 +1,48 @@
 #include "Soundex.hpp"
 #include <stdexcept>
-#include  <cctype>
+#include <cctype>
 #include <algorithm>
 #include <vector>
-
+#include <map>
 
 std::string Soundex::doSoundex(const std::string input)
 {
-    if(input.empty())
+    if (input.empty())
     {
         throw std::runtime_error("Input is empty");
     }
 
-    if (std::any_of(input.cbegin(), input.cend(), [](auto c){ return !std::isalpha(c); })) {
+    if (std::any_of(input.cbegin(), input.cend(), [](auto c) { return !std::isalpha(c); }))
+    {
         throw std::runtime_error("Input contains no alpha value.");
     }
 
-    std::string result {input[0]};
+    std::string result{input[0]};
 
-    std::vector<char> cypher = {'C', 'G', 'J', 'K', 'Q', 'S', 'X', 'Z'};
-    std::vector<char> cypher2 = {'B', 'F', 'P', 'V'};
-    for (auto e : input)
-    {
-        auto it = std::find(cypher.begin(), cypher.end(), e);
-        if(it != cypher.end() and e != input.front() ) {
-            result += "2";
-        }
+    std::map<char, int> cypher_map{
+        {'C', 2},
+        {'G', 2},
+        {'J', 2},
+        {'K', 2},
+        {'Q', 2},
+        {'S', 2},
+        {'X', 2},
+        {'Z', 2},
+        {'B', 1},
+        {'F', 1},
+        {'P', 1},
+        {'V', 1}};
 
-        it = std::find(cypher2.begin(), cypher2.end(), e);
-        if(it != cypher2.end() and e != input.front() ) {
-            result += "1";
-        }
+    for (int i=1;i<input.size();i++) {
+        result += std::to_string(cypher_map[input[i]]);
     }
+
     int s = result.size();
-    for(int i = 0; i < 4-s; i++)
+    for (int i = 0; i < 4 - s; i++)
     {
-        result+="0";
+        result += "0";
     }
 
     return result;
- 
-    //return input + "000";
+
 }
